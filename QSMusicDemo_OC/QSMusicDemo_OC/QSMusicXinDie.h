a3d3581869fe79fa8d4cadd0024eb37b9556f262
qqqssagithub <chenshaowen@chenshaowendeMac-mini.local>
2016-08-20 00:22:50 +0800

    更新单元测试

--- QSMusicDemo_OC/QSMusicDemo_OC/QSMusicXinDie.h
+++ QSMusicDemo_OC/QSMusicDemo_OC/QSMusicXinDie.h
@@ -15,6 +15,12 @@
 
 + (instancetype)sharedQSMusicXinDie;
 
+/**
+ *  单元测试用的接口，外部请勿调用
+ *
+ *  @param albumId
+ *  @param completion
+ */
 - (void)requestSingleWithAlbumId:(NSString *)albumId response:(void(^)(NSDictionary *albumInfo, NSArray *songList))completion;
 
 @end
